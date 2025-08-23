6b4c979e0c05a21ffa0761fb48692ba6236a962a
ImcoVeenstra <imco@mett.nl>
2025-08-23 20:47:13 +0200

    Added some images + added Algemene voorwaarden

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,12 @@
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 export default async function (eleventyConfig)
 {
 	eleventyConfig.setInputDirectory("./src/");
 	eleventyConfig.setOutputDirectory("./dist/");
 	eleventyConfig.addPlugin(eleventyNavigationPlugin);
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 	eleventyConfig.addWatchTarget("css/**/*.css");
 	eleventyConfig.addPassthroughCopy( { "css/index.css": "index.css"});
 
---
+++ src/algemente-voorwaarden.md
@@ -0,0 +1,9 @@
+---
+layout: layout.html
+tags: footer
+title: Algemene voorwaarden Jappie's Putsjes Bedriuw
+eleventyNavigation:
+  key: Algemene voowaarden
+  order: 2
+---
+Download hier de algemene voorwaarden
